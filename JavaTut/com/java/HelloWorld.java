b4d75eb3ec9d76bbb6c6dce16dcf6821a6ab3a95
BhushanMasne <bhushan.masne@gmail.com>
2016-09-18 00:12:00 +0530

    Updates

--- JavaTut/com/java/HelloWorld.java
+++ JavaTut/com/java/HelloWorld.java
@@ -3,6 +3,6 @@ package com.java;
 public class HelloWorld {
 	 public static void main(String args[]){
 	 System.out.println("Bhushan 1");
-	 System.out.println("Bhushan 2");
+	 System.out.println("Bhushan 3");
 	 }
 }
